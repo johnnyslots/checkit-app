da30d8dad1532ec868554028d63f795f3a975bae
Yoni Slotwinere <yonislots@gmail.com>
2018-06-08 18:16:06 -0400

    fixed date issue in recDetails component

--- components/screens/RecDetails.js
+++ components/screens/RecDetails.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { View, Text, Linking } from 'react-native';
-import months from '../utils';
+import { months } from '../utils';
 
 export default class RecDetails extends React.Component {
   render() {
     const recDetails = this.props.navigation.state.params.rec;
-    const recommender = recDetails.from ? recDetails.from : 'me';
+    const recommender = recDetails.from ? recDetails.from.fullName : 'me';
     const url = recDetails.item.findOnGoogle
     const dateSplit = recDetails.createdAt.slice(0, 10).split('-');
-    console.log('DATE', dateSplit, months)
     const recommendedAt = `${months[dateSplit[1]]} ${dateSplit[2]}, ${dateSplit[0]}`;
 
     return(
       <View>
         <Text>{recDetails.item.title}</Text>
-        <Text>Recommended by {recommender} on </Text>
+        <Text>Recommended by {recommender} on {recommendedAt}</Text>
         <Text>Notes: {recDetails.notes}</Text>
         <Text onPress={() => Linking.openURL(url)}>Find it on Google</Text>
       </View>
