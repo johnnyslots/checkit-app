ae869e44b98b6151e09e024fd78c5344fbb03900
Yoni Slotwinere <yonislots@gmail.com>
2018-06-12 13:04:32 -0400

    beginnings of request rec form

--- components/screens/RequestRec.js
+++ components/screens/RequestRec.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { Dropdown } from 'react-native-material-dropdown';
+import { TextField } from 'react-native-material-textfield';
 
 export default class RequestRec extends React.Component {
   render() {
     return (
       <View>
         <Text>REQUEST REC</Text>
+          <Dropdown
+            label='Category'
+            // data={categories}
+            // containerStyle={sendRecStyles.input}
+            // onChangeText={this.handleCategoryChange}
+          />
+          <TextField
+            // containerStyle={sendRecStyles.input}
+            // onChangeText={this.handleEmailChange}
+            // value={this.state.email}
+            label="Send to (email)"
+          />
+          {/*
+            this.state.incorrectEmail ?
+            <Text>User doesn't exist</Text>
+            : null
+          */}
+          <TextField
+            // containerStyle={sendRecStyles.input}
+            // onChangeText={this.handleTitleChange}
+            // value={this.state.title}
+            label="Title"
+          />
+          <TextField
+            // containerStyle={sendRecStyles.input}
+            // onChangeText={this.handleNotesChange}
+            // value={this.state.notes}
+            label="Notes"
+          />
       </View>
     )
   }
