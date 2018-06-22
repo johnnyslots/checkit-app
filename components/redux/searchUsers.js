b1d693626dd4b58be0912368f1d62f36067cfa4e
Yoni Slotwinere <yonislots@gmail.com>
2018-06-21 21:22:43 -0400

    search functionality working

--- components/redux/searchUsers.js
+++ components/redux/searchUsers.js
@@ -5,19 +5,21 @@ const GET_USERS = 'GET_USERS';
 
 export const getUsers = users => ({type: GET_USERS, users})
 
-export const fetchUsers = (input) => dispatch => {
-  axios.get(`${IP}/api/users/search/${input}`)
-  .then(res => res.data)
-  .then(users => {
-    if(users) {
-      dispatch(getUsers(users))
-    }
-    else {
-      let noUsers = []
-      dispatch(getUsers(noUsers))
-    }
-  })
-  .catch(err => console.log(err))
+export const fetchUsers = (input, currentUserId) => dispatch => {
+  if(input) {
+    axios.get(`${IP}/api/users/${currentUserId}/search/${input}`)
+    .then(res => res.data)
+    .then(users => {
+      if(users) {
+        dispatch(getUsers(users))
+      }
+      else {
+        let noUsers = []
+        dispatch(getUsers(noUsers))
+      }
+    })
+    .catch(err => console.log(err))
+  }
 }
 
 export default function reducer(users = [], action) {
--- components/screens/SearchUsers.js
+++ components/screens/SearchUsers.js
@@ -3,16 +3,43 @@ import { StyleSheet, View, Text } from 'react-native';
 import { SearchBar, List, ListItem, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { fetchUsers } from '../redux/searchUsers';
+import axios from 'axios';
+import IP from '../../secrets';
 
 class SearchUsers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pendingOrRejected: [],
+      accepted: []
+    }
+    this.addUser = this.addUser.bind(this);
+  }
+
+  addUser(userId) {
+    const currentUserId = this.props.currentUser.id
+    axios.post(`${IP}/api/friends/requests`, {userId, currentUserId})
+    .then(res => res.data)
+    .then(friendStatus => {
+      if(friendStatus.status === 'pending' || friendStatus.status === 'rejected') {
+        this.setState({pendingOrRejected: [...this.state.pendingOrRejected, friendStatus.friendId]})
+      }
+      else if(friendStatus.status === 'accepted') {
+        this.setState({accepted: [...this.state.accepted, friendStatus.friendId]})
+      }
+    })
+    .catch(err => console.log(err))
+  }
+
   render() {
-    const { fetchUsers, users } = this.props
+    const { fetchUsers, users, currentUser } = this.props
+    const { pendingOrRejected, accepted } = this.state
+
 
     return (
       <View>
         <SearchBar
-          onChangeText={(input) => fetchUsers(input)}
-          // onClearText={someMethod}
+          onChangeText={(input) => fetchUsers(input, currentUser.id)}
           icon={{ type: 'font-awesome', name: 'search' }}
           placeholder='Search for friends by email...'
         />
@@ -21,20 +48,52 @@ class SearchUsers extends React.Component {
           <List>
             {
               users.map(user => {
-                return (
-                  <View key={user.id}>
-                    <ListItem
-                      title={user.fullName}
-                      rightIcon={
-                        <Icon
-                          name={'add'}
-                          size={20}
-                          onPress={() => console.log('Pressed !')}
-                        />
-                      }
-                    />
-                  </View>
-                )
+                if(pendingOrRejected.indexOf(user.id) > -1) {
+                  return (
+                    <View key={user.id}>
+                      <ListItem
+                        title={user.fullName}
+                        rightIcon={
+                          <Icon
+                            name={'access-time'}
+                            size={20}
+                          />
+                        }
+                      />
+                    </View>
+                  )
+                }
+                else if(accepted.indexOf(user.id) > -1) {
+                  return (
+                    <View key={user.id}>
+                      <ListItem
+                        title={user.fullName}
+                        rightIcon={
+                          <Icon
+                            name={'check'}
+                            size={20}
+                          />
+                        }
+                      />
+                    </View>
+                  )
+                }
+                else {
+                  return (
+                    <View key={user.id}>
+                      <ListItem
+                        title={user.fullName}
+                        rightIcon={
+                          <Icon
+                            name={'add'}
+                            size={20}
+                            onPress={() => this.addUser(user.id)}
+                          />
+                        }
+                      />
+                    </View>
+                  )
+                }
               })
             }
           </List>
@@ -46,12 +105,12 @@ class SearchUsers extends React.Component {
 }
 
 const mapStateToProps = state => ({
+  currentUser: state.currentUser,
   users: state.users
 })
 
 const mapDispatchToProps = dispatch => ({
-  fetchUsers: (input) => dispatch(fetchUsers(input))
+  fetchUsers: (input, currentUserId) => dispatch(fetchUsers(input, currentUserId))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchUsers)
-
